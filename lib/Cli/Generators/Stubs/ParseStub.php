2ff542e7d6cc7bb57968aa902f0cfa2491865c24
mark <ma_fengtao@163.com>
2017-08-08 23:06:03 +0800

    目录分隔符修改

--- lib/Cli/Generators/Stubs/ParseStub.php
+++ lib/Cli/Generators/Stubs/ParseStub.php
@@ -60,19 +60,19 @@ class ParseStub
             case 'model':
                 $this->name = 'model.stub';
                 $this->class_namespace = 'App\Models';
-                $this->target_dir = 'app\Models';
+                $this->target_dir = 'app'. DIRECTORY_SEPARATOR .'Models';
                 $this->target_suffix = '';
                 break;
             case 'controller':
                 $this->name = 'controller.stub';
                 $this->class_namespace = 'App\Controllers';
-                $this->target_dir = 'app\Controllers';
+                $this->target_dir = 'app'. DIRECTORY_SEPARATOR .'Controllers';
                 $this->target_suffix = 'Controller';
                 break;
             case 'command':
                 $this->name = 'command.stub';
                 $this->class_namespace = 'App\Console\Commands';
-                $this->target_dir = 'app\Console\Commands';
+                $this->target_dir = 'app'. DIRECTORY_SEPARATOR .'Console'. DIRECTORY_SEPARATOR.'Commands';
                 $this->target_suffix = 'Command';
                 break;
             default:
@@ -87,17 +87,17 @@ class ParseStub
      */
     public function parseArgument(){
         if($this->argument){
-            if(strpos($this->argument, '\\') === false){
+            if(strpos($this->argument, DIRECTORY_SEPARATOR) === false){
                 $this->class_name = ucfirst($this->argument);
-                $this->target_name = $this->target_dir . '\\' . $this->class_name;
+                $this->target_name = $this->target_dir . DIRECTORY_SEPARATOR . $this->class_name;
             }else{
-                $argument_arr = explode('\\', $this->argument);
+                $argument_arr = explode(DIRECTORY_SEPARATOR, $this->argument);
                 $this->class_name = ucfirst(end($argument_arr));
-                $this->target_name = $this->target_dir . '\\';
+                $this->target_name = $this->target_dir . DIRECTORY_SEPARATOR;
                 foreach($argument_arr as $k => $arg){
                     if($k != count($argument_arr)){
-                        $this->class_namespace .= '\\' . $arg;
-                        $this->target_name .= '\\' . ucfirst($arg);
+                        $this->class_namespace .= DIRECTORY_SEPARATOR . $arg;
+                        $this->target_name .= DIRECTORY_SEPARATOR . ucfirst($arg);
                     }
                 }
             }
@@ -110,7 +110,7 @@ class ParseStub
      */
     public function make(){
         $this->parseArgument();
-        $stub_file = $this->dir . '/' . $this->name;
+        $stub_file = $this->dir . DIRECTORY_SEPARATOR . $this->name;
         if(file_exists($stub_file)){
             $content = file_get_contents($stub_file);
             $content = str_replace('$CLASS$', $this->class_name . $this->target_suffix, $content);
